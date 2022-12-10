e9b938980315717a89a60868c1737c1b13ffb4ed
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2022-12-10 16:09:04 +0100

    Day 2

--- src/02/index.ts
+++ src/02/index.ts
@@ -1,43 +1,18 @@
 import { add, getLinesFromInput } from '../utils'
 
-type Strategy = `${'A' | 'B' | 'C'}${'X' | 'Y' | 'Z'}`
-type PointsMap = Record<Strategy, number>
+const lines = getLinesFromInput(__dirname).map(x => x.replace(' ', ''))
 
-const W = 6 // WIN
-const D = 3 // DRAW
-const L = 0 // LOSE
-
-const X = 1 // A - ROCK
-const Y = 2 // B - PAPER
-const Z = 3 // C - SCISSORS
-
-const strategies = getLinesFromInput(__dirname).map(
-  line => line.replace(' ', '') as Strategy
-)
-
-const pointsMap: PointsMap = {
-  AX: D + X,
-  AY: W + Y,
-  AZ: L + Z,
-  BX: L + X,
-  BY: D + Y,
-  BZ: W + Z,
-  CX: W + X,
-  CY: L + Y,
-  CZ: D + Z,
+const points: Record<string, number[]> = {
+  AX: [4, 3],
+  AY: [8, 4],
+  AZ: [3, 8],
+  BX: [1, 1],
+  BY: [5, 5],
+  BZ: [9, 9],
+  CX: [7, 2],
+  CY: [2, 6],
+  CZ: [6, 7],
 }
 
-const pointsMap2: PointsMap = {
-  AX: L + Z,
-  AY: D + X,
-  AZ: W + Y,
-  BX: L + X,
-  BY: D + Y,
-  BZ: W + Z,
-  CX: L + Y,
-  CY: D + Z,
-  CZ: W + X,
-}
-
-console.log('Part 1:', strategies.map(s => pointsMap[s]).reduce(add))
-console.log('Part 2:', strategies.map(s => pointsMap2[s]).reduce(add))
+console.log('Part 1:', lines.map(s => points[s][0]).reduce(add))
+console.log('Part 2:', lines.map(s => points[s][1]).reduce(add))
